ffa81efdb2e21cd9901ca493145048f429e2f18b
Abdullah Al Mamun <almamun00021@gmail.com>
2025-03-10 20:49:57 +0600

    Update route.ts

--- src/app/api/hotels/[id]/route.ts
+++ src/app/api/hotels/[id]/route.ts
@@ -1,28 +1,22 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// Get a single hotel
-export async function GET(req: Request, 
-  { params }: { params: { id: string } }) {
-   
-  if (!params || !params.id) {
-    return NextResponse.json({ error: "Hotel ID is required" }, { status: 400 });
-  }
-
+export async function GET(req: NextRequest, context: { params: { id: string } }) {
   try {
     const hotel = await prisma.hotel.findUnique({
-      where: { id: Number(params.id) },
+      where: { id: Number(context.params.id) }, // Ensure ID is converted to a number if needed
     });
 
     if (!hotel) {
-      return NextResponse.json({ error: "Hotel not found" }, { status: 404 });
+      return NextResponse.json({ error: 'Hotel not found' }, { status: 404 });
     }
 
     return NextResponse.json(hotel, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    console.error('Error fetching hotel:', error);
+    return NextResponse.json({ error: 'Failed to fetch hotel' }, { status: 500 });
   }
 }
 
