7bccb153037e13ac3ca111e1c5a919e82c60e5ec
abdullahmamunn <almamun00021@gmail.com>
2025-03-11 12:25:59 +0600

    some chnages

--- src/app/api/hotels/[id]/route.ts
+++ src/app/api/hotels/[id]/route.ts
@@ -4,16 +4,17 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 // Get a single hotel
-export async function GET(req: Request, 
-  { params }: { params: { id: string } }) {
-   
-  if (!params || !params.id) {
+export async function GET(request: Request,
+   { params }: { params: Promise < { id: string }> }) {
+     const { id } = await params;
+
+  if (!id) {
     return NextResponse.json({ error: "Hotel ID is required" }, { status: 400 });
   }
 
   try {
     const hotel = await prisma.hotel.findUnique({
-      where: { id: Number(params.id) },
+      where: { id: Number(id) },
     });
 
     if (!hotel) {
@@ -27,18 +28,18 @@ export async function GET(req: Request,
 }
 
 // Update hotel details
-export async function PUT(req: Request, context: { params: { id: string } }) {
-  const params = await context.params;
+export async function PUT(request: Request, { params }: { params: Promise<{id: string }> }) {
+  const {id} = await params;
 
-  if (!params || !params.id) {
+  if (!params || !id) {
     return NextResponse.json({ error: "Hotel ID is required" }, { status: 400 });
   }
 
   try {
-    const { name, address, costPerNight, availableRooms, imageUrl, rating } = await req.json();
+    const { name, address, costPerNight, availableRooms, imageUrl, rating } = await request.json();
 
     const updatedHotel = await prisma.hotel.update({
-      where: { id: Number(params.id) },
+      where: { id: Number(id) },
       data: { name, address, costPerNight, availableRooms, imageUrl, rating },
     });
 
@@ -49,10 +50,10 @@ export async function PUT(req: Request, context: { params: { id: string } }) {
 }
 
 // Delete a hotel
-export async function DELETE(req: Request, context: { params: { id: string } }) {
-  const params = await context.params; 
+export async function DELETE(request: Request, { params }: { params: Promise<{ id: string }> }) {
+  const {id} = await params; 
   await prisma.hotel.delete({
-    where: { id: Number(params.id) },
+    where: { id: Number(id) },
   });
 
   return NextResponse.json({ message: "Hotel deleted successfully" }, { status: 200 });
